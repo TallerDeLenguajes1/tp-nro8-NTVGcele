94e5084d042ae45983ce4202558bc707e9c8ee1d
Gonzalo <querer44@gmail.com>
2019-05-30 17:18:10 -0300

    tp8 terminado

---
+++ tp8/tp8/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp8
+{
+    public class Empleado
+    {
+        public enum Nombre { Fabio, Alex, Fede, silvia }
+        public enum Apellido { Soto, Cordoba, Perez, Mamani }
+        public enum Trabajo { Auxiliar, Administrativo, Ingeniero, Especialista, Investigador }
+        public enum Estado { casado, soltero }
+        public enum Genero { femenino, masculino }
+
+
+
+        // fechanac = new DateTime(1,5,1998);
+
+       public Nombre nombre;
+       public Apellido apellido;
+       public DateTime fechNac;//fecha de nacimiento
+       public Estado civil;
+       public Genero sexo;
+       public DateTime fechIngreso; //fecha de ingreso a la empresa
+       public double sueldoB;
+       public Trabajo cargo;
+       public int edad;
+       public int antiguedad;
+       public int jub;//jubilacion
+       public double salario;
+
+        public void cargarDatos ()
+        {
+            Random aleatorio = new Random();
+            nombre = (Nombre)aleatorio.Next(0, 4);
+            apellido = (Apellido)aleatorio.Next(0, 4);
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = ((TimeSpan)(DateTime.Today - start)).Days;
+            fechNac = start.AddDays (aleatorio.Next(range));
+            civil = (Estado)aleatorio.Next(0, 2);
+            sexo = (Genero)aleatorio.Next(0, 2);
+            fechIngreso = start.AddDays(aleatorio.Next(range));
+            sueldoB = aleatorio.Next(0, 5000);
+            cargo = (Trabajo)aleatorio.Next(0, 5);
+            antiguedad = DateTime.Today.AddTicks(-fechIngreso.Ticks).Year ;
+            edad = DateTime.Today.AddTicks(-fechNac.Ticks).Year - 1;
+            switch (sexo)
+            {
+                case 0: jub = DateTime.Today.AddTicks(- fechNac.Ticks).Year - 60; break;
+                case (Genero)1:  jub = DateTime.Today.AddTicks(- fechNac.Ticks).Year - 65; break;
+            }
+            //CALCULO EL SALARIO
+            double adicional;
+            if ( antiguedad < 21)
+            {
+                
+                adicional = ((sueldoB * 0.02) * (antiguedad));
+                switch (cargo)
+                {
+                    case (Trabajo)2:
+                    case (Trabajo)3: adicional = +(adicional * 0.5); break;
+                        //default:;break;
+                }
+                switch (civil)
+                {
+                    case (Estado)0: adicional = +50000; break;
+                }
+                salario = sueldoB + adicional;
+            }
+            else
+            {
+                adicional = (sueldoB * 0.25);
+                salario = adicional + sueldoB;
+            }
+        }
+      
+    }
+}
---
+++ tp8/tp8/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Empleado> listaEmpleados = new List<Empleado>();
+            int cant = 2;
+            //Console.Write("ingrese la cantidad de empleados: ");
+            //cant = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cant; i++)
+            {
+                Empleado nuevo = new Empleado();
+                nuevo.cargarDatos();
+                listaEmpleados.Add(nuevo);
+            }
+            int e = 1;
+            string EmpleadoTexto = "";
+            string AgregarText = "";//Para agregar texto en el archivo creado
+            foreach (Empleado lectura in listaEmpleados)
+            {
+                
+                Console.Write("\n\n PERSONA: " + e);
+                EmpleadoTexto += lectura.nombre.ToString() + ";";
+                AgregarText += lectura.nombre.ToString() + ";";
+                Console.Write("\n Nombre: {0}", lectura.nombre);
+
+                EmpleadoTexto += lectura.apellido.ToString() + ";";
+                AgregarText += lectura.apellido.ToString() + ";";
+                Console.Write("\n Apellido: " + lectura.apellido);
+
+                EmpleadoTexto += lectura.fechNac.ToString() + ";";
+                AgregarText += lectura.fechNac.ToString() + ";";
+                Console.Write("\n Fecha de nacimiento: " + lectura.fechNac);
+
+                EmpleadoTexto += lectura.civil.ToString() + ";";
+                AgregarText += lectura.civil.ToString() + ";";
+                Console.Write("\n Estado Civil: " + lectura.civil);
+
+                EmpleadoTexto += lectura.sexo.ToString() + ";";
+                AgregarText += lectura.sexo.ToString() + ";";
+                Console.Write("\n Sexo: " + lectura.sexo);
+
+                EmpleadoTexto += lectura.fechIngreso.ToString() + ";";
+                AgregarText += lectura.fechIngreso.ToString() + ";";
+                Console.Write("\n Fecha de ingreso a la empresa: " + lectura.fechIngreso);
+
+                EmpleadoTexto += lectura.sueldoB.ToString() + ";";
+                AgregarText += lectura.sueldoB.ToString() + ";";
+                Console.Write("\n Sueldo basico: " + lectura.sueldoB);
+
+                EmpleadoTexto += lectura.cargo.ToString() + ";";
+                AgregarText += lectura.cargo.ToString() + ";";
+                Console.Write("\n Cargo: " + lectura.cargo);
+
+                EmpleadoTexto += lectura.edad.ToString() + ";";
+                AgregarText += lectura.edad.ToString() + ";";
+                Console.Write("\n Edad: " + lectura.edad);
+
+                EmpleadoTexto += lectura.antiguedad.ToString() + ";";
+                AgregarText += lectura.antiguedad.ToString() + ";";
+                Console.Write("\n Antiguedad: " + lectura.antiguedad);
+
+                EmpleadoTexto += lectura.salario.ToString() + Environment.NewLine;
+                AgregarText += lectura.salario.ToString() + Environment.NewLine;
+                Console.Write("\n Salario: " + lectura.salario);
+                //EmpleadoTexto += "\n";
+                e = e + 1;
+            }
+            System.Console.ReadKey();
+
+            Console.ReadKey();
+            // UTILIZO SYSTEM.IO.Directory
+            string BuscandoCarpeta = @"c:\repo8\";
+            if (!Directory.Exists(BuscandoCarpeta))
+            {
+                Directory.CreateDirectory(BuscandoCarpeta);
+
+            }
+            Console.WriteLine();
+
+
+            string rutaDeAchivo = @"c:\repo8\Prueba.CVS";
+            // UTILIZO SYSTEM.IO.file
+            //File.Create(rutaDeAchivo); // Crea un archivo Prueba .csv
+
+            //if (File.Exists(rutaDeAchivo)) //Comprueba si existe el archivo
+            //{
+               //ESCRIBI EN EL ARCHIVO
+               /* File.WriteAllText(rutaDeAchivo, EmpleadoTexto);
+                Console.WriteLine("Existe Prueba.CVS");*/
+            //}
+            //AGREGO TEXTO EN EL ARCHIVO CRECADO
+            string agregar = AgregarText + Environment.NewLine;
+            File.AppendAllText(rutaDeAchivo, agregar);
+           // string readText = File.ReadAllText(rutaDeAchivo);
+            //Console.WriteLine(readText);
+
+            string [] disco = { @"c:\BackUpAgenda", @"d:\BackUpAgenda" };
+            int P;
+            Console.Write("Escriba 0 si desea guardar el archivo en Disco C o 1 si desea guardar el archivo en Disco E: ");
+            P = int.Parse(Console.ReadLine());
+            if(!Directory.Exists(disco[P])){
+                Directory.CreateDirectory(disco[P]);
+            }
+            string rutaNuevaCarpeta = disco[P] + @"\copia.CVS";
+            string agregarcopia = AgregarText + Environment.NewLine;
+            File.AppendAllText(rutaNuevaCarpeta, agregarcopia);
+            Console.Write("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+
+            string readTextcopia = File.ReadAllText(rutaNuevaCarpeta);
+
+            Console.WriteLine(readTextcopia);
+
+
+            Console.ReadKey();
+        }
+    }
+}
